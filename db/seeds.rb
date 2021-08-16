ba539ddc8089add4f28688e73a622718962ccc18
Ricardo Dupot <ricawdo@gmail.com>
2021-08-16 19:03:11 +0200

    added seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+puts 'Creating wadi...'
+
+user = User.new(
+    first_name: "Wadi",
+    last_name: "Boom",
+    email: "wadiboom@gmail.com",
+    password: "123456"
+ )
+user.save!
+
+puts 'Creating user...'
+10.times do
+  user = User.new(
+    first_name: Faker::Name,
+    last_name: Faker::Name,
+    email: Faker::Internet.email,
+    password: "123456"
+  )
+  user.save!
+end
+puts 'Finished!'
