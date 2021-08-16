baa56c810e5d9c471b5da929154771935a071012
Ricardo Dupot <ricawdo@gmail.com>
2021-08-16 18:20:59 +0200

    almost done

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,25 @@
 class AnimalsController < ApplicationController
+  def new
+    @animal = Animal.all
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    @animal.user = @user
+    if @animal.save
+      redirect_to animals_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def animal_params
+    params.require(:animal).permit(:name, :breed, :age, :price, :description, :specie, :adress, :poster_url)
+  end
 end
