fee12906c3ee58a14b0185b5e080504749a345eb
gwenael marie <gwen.marie04@gmail.com>
2021-08-17 14:30:23 +0200

    rout + controller ok debut de view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,24 @@
 class BookingsController < ApplicationController
+  def new
+    @animal = Animal.find(params[:animal_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @animal = Animal.find(params[:animal_id])
+    @booking.animal = @animal
+    @booking.status = false
+    if @booking.save
+      redirect_to animal_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
