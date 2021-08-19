b384576eb7e66222d72cd19873111ba0d2dd0bf9
Ricardo Dupot <ricawdo@gmail.com>
2021-08-19 18:28:27 +0200

    almost done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,6 +36,10 @@ import { initStarRating } from '../plugins/init_star_rating';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  navigator.geolocation.getCurrentPosition((data) => {
+    console.log(data);
+    });
+
   const mapElement = document.getElementById('map');
   if (mapElement) {
     initMapbox();
