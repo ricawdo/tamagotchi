f7dcdc55ddc57e9005cf726436c8c03c7ce74b10
gwenael marie <gwen.marie04@gmail.com>
2021-08-20 14:35:42 +0200

    changed language in home

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,7 +3,7 @@ class Animal < ApplicationRecord
   has_many :bookings
   has_one_attached :photo
   # SPECIE for "rent your pet", to do later
-  SPECIE = %w(chat chien poule poisson )
+  SPECIE = %w(Cat Dog Chicken Fish )
 
   geocoded_by :adress
   after_validation :geocode, if: :will_save_change_to_adress?
